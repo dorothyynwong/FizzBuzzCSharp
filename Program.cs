a0218ec68eddf42820ee89a5a78df11749957a11
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-05 11:17:01 +0100

    Task 4 with Enumerable.Range

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 namespace FizzBuzz
 {
     class Program
@@ -6,16 +7,27 @@ namespace FizzBuzz
 
         static void Main(string[] args)
         {
-            string result = "";
-            for(int i=1; i<=100; i++) {
-                string word = "";
-                if (i%3 == 0) word += "Fizz"; 
-                if  (i%5 == 0) word += "Buzz";
-                if (i%3 !=0 && i%5 !=0) word = i.ToString();
+            // string result = "";
+            // for(int i=1; i<=100; i++) {
+            //     string word = "";
+            //     if (i%3 == 0) word += "Fizz"; 
+            //     if  (i%5 == 0) word += "Buzz";
+            //     if (i%3 !=0 && i%5 !=0) word = i.ToString();
 
-                result += word + " ";
-            }
-            Console.WriteLine(result.TrimEnd());
+            //     result += word + " ";
+            // }
+            // Console.WriteLine(result.TrimEnd());
+
+            IEnumerable<String> numbers = Enumerable.Range(1, 100).Select(num => {
+                   if (num%3 ==0 && num%5 ==0) return "FizzBuzz";
+                   if (num%3 == 0) return "Fizz"; 
+                   if (num%5 == 0) return "Buzz";
+                   return num.ToString();
+            });
+
+            string result = string.Join(" ", numbers);
+
+            Console.WriteLine(result);
         }
     }
 }
