ca874a0ef853e186ccd4c2d4e697e84ac009b59f
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-05 12:10:01 +0100

    added rule for 13

--- Program.cs
+++ Program.cs
@@ -20,11 +20,21 @@ namespace FizzBuzz
                 // {35, "BuzzBang"}
             };
 
+
             IEnumerable<String> numbers = Enumerable.Range(1, 100).Select(num => {
                 List<string> word = new List<string>();
+
+                if (num%13 ==0) word.Add(rules[13]);
+
                 foreach (var rule in rules)
                 {
-                    if (num % rule.Key == 0) word.Add(rule.Value);
+                    if (rule.Key != 13 && num % rule.Key == 0) word.Add(rule.Value);
+                }
+
+                if (word.Contains(rules[13]) && word.Contains(rules[3])) {
+                    word.Remove(rules[13]);
+                    int fizzIndex = word.IndexOf(rules[3]);
+                    word.Insert(fizzIndex + 1, rules[13]);
                 }
 
                 if (word.Count == 0) word.Add(num.ToString());
