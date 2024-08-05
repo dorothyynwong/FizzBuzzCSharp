b62207b71e371bd906ced7bb23be7a2bbcea5e1b
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-05 10:55:38 +0100

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = String.Join(" ", Enumerable.Range(1, 100));
+            Console.WriteLine(result);
+        }
+    }
+}
