f9da74a10ba109ad1ed80bcaced32343252811c9
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-05 11:03:47 +0100

    Task 4

--- Program.cs
+++ Program.cs
@@ -3,17 +3,19 @@ namespace FizzBuzz
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             string result = "";
             for(int i=1; i<=100; i++) {
                 string word = "";
-                if (i%3 == 0) word = "Fizz"; 
-                else word = i.ToString();
+                if (i%3 == 0) word += "Fizz"; 
+                if  (i%5 == 0) word += "Buzz";
+                if (i%3 !=0 && i%5 !=0) word = i.ToString();
 
-                result = String.Join(" ", result, word);
+                result += word + " ";
             }
-            Console.WriteLine(result);
+            Console.WriteLine(result.TrimEnd());
         }
     }
 }
