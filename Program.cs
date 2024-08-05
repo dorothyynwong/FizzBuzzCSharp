eef3384482809db344b59a470d03a42bf444c424
Dorothy Wong <dorothyynwong@gmail.com>
2024-08-05 11:41:00 +0100

    try using dictionary,working on rule 13

--- Program.cs
+++ Program.cs
@@ -12,17 +12,23 @@ namespace FizzBuzz
             Dictionary<int, string> rules = new Dictionary<int, string>{
                 {3, "Fizz"},
                 {5, "Buzz"},
-                {15, "FizzBuzz"}
+                {7, "Bang"},
+                {11, "Bong"},
+                {13, "Fezz"},
+                {15, "FizzBuzz"},
+                {21, "FizzBang"},
+                {35, "BuzzBang"}
             };
 
             IEnumerable<String> numbers = Enumerable.Range(1, 100).Select(num => {
                 string word = "";
-                   foreach(var rule in rules) {
+                foreach (var rule in rules)
+                {
                     if (num % rule.Key == 0) word = rule.Value;
-                   }
+                }
 
-                   if(string.IsNullOrEmpty(word)) word = num.ToString();
-                   return word;
+                if (string.IsNullOrEmpty(word)) word = num.ToString();
+                return word;
             });
 
             string result = string.Join(" ", numbers);
